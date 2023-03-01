ec6aa7bd597aabf3e164a4dce92f27806536b7be
sahill-1 <sv260397@gmail.com>
2023-03-01 12:25:01 +0530

    25 this.state

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
-      let {title, description} = this.props
+      let {title, description, imgUrl, newsUrl} = this.props
         return (
-            <div>
+            <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80" className="card-img-top" alt="..." />
+                    <img src={imgUrl} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
+                            <a href="/" className="btn btn-sm btn-primary">Read More</a>
                         </div>
                 </div>
             </div>
